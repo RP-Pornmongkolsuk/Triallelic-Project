a3f7fe1785ddc2a27627dca0d634c137c1b911cb
RP Pornmongkolsuk <82060975+RP-Pornmongkolsuk@users.noreply.github.com>
2021-04-24 00:27:55 -0400

    Update runfinal.sh

--- runfinal.sh
+++ runfinal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for CHR in {1..22}; do
-sbatch -J ${CHR}SNPsResult -N 1 -n 4 -t 1-0 --mem=2G -o ${CHR}SNPsResult-%A.out -e ${CHR}SNPsResult-%A.err --mail-type=ALL --mail-user=ratchanon_pornmongkolsuk@brown.edu --wrap="python ${CHR}TriallelicSNPs.py"
+sbatch -J ${CHR}SNPsResult -N 1 -n 6 -t 1-0 --mem=10G -o ${CHR}SNPsResult-%A.out -e ${CHR}SNPsResult-%A.err --mail-type=ALL --mail-user=ratchanon_pornmongkolsuk@brown.edu --wrap=“python ${CHR}TriallelicSNPs.py”
 done
